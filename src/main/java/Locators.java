057010bc9a049dd94ab4b0d20e67fa3d1806a288
cris-cod <aarevalo.cristian.17@gmail.com>
2024-04-17 21:21:39 -0500

    add new locators

--- src/main/java/Locators.java
+++ src/main/java/Locators.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Locators {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //Implicit wait -- 2seconds time out
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
@@ -17,13 +17,45 @@ public class Locators {
         driver.findElement(By.className("signInBtn")).click();
 
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Jonh");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("jonh@ser.com");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
+        driver.findElement(By.xpath("//input[@type = 'text'][2]")).sendKeys("jonhDoe@ser.com");
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("365555993");
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+        driver.findElement(By.xpath("//div[@class = 'forgot-pwd-btn-conainer']/button[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+        driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.id("chkboxOne")).click();
+        driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+
+
 
 
         //Css Selector
-        //class name -> tagname.classname  - p.error
-        //ic -> tagname#id -> input<
-        //Tagname[attribute='value']
-        //input[placeholder = 'username']
+        // class name -> tagname.classname  - p.error
+        // ic -> tagname#id -> input<
+        // Tagname[attribute='value']   ---- input[placeholder='Email']
+        // input[placeholder = 'username']
+        // input[type='text']:nth-child(3)
+        // .reset-pwd-btn   --- .classname
+        // form p          ----- parentagname childtagname
+
+        // Regular expresion - input[type*='pass']
+
+        /*
+        * Xpath
+        *  //Tagname[@attribute='value']
+        *  //input[@placeholder='value']  -- //input[@placeholder='Name']
+        *  //input[@type = 'text'][2]
+        *  //form/input[3]
+        *  //button[contains(@class, 'submit')]   --- Regular expresion
+        *  //div[@class = 'forgot-pwd-btn-conainer']/button[1]
+        *         * */
 
     }
 }
